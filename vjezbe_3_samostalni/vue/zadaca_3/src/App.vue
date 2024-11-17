ab9043fc3bd3e700b10902924db914a1672421bf
Juraj Štern-Vukotić <jsternvuk@unipu.hr>
2024-11-17 22:30:51 +0100

    add template

--- vjezbe_3_samostalni/vue/zadaca_3/src/App.vue
+++ vjezbe_3_samostalni/vue/zadaca_3/src/App.vue
@@ -1,22 +1,9 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    <!-- Uključujemo komponentu -->
+    <ProductView />
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import ProductView from "./components/Proizvod.vue";
+</script>
