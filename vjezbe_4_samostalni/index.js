a422b4dfeb768c4fbab83a62ce22622c823c9b42
Juraj Štern-Vukotić <jsternvuk@unipu.hr>
2024-11-23 20:25:25 +0100

    setup

---
+++ vjezbe_4_samostalni/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    console.error(`Greška prilikom pokretanja poslužitelja: ${error.message}`);
+  } else {
+    console.log(`Server dela na http://localhost:${PORT}`);
+  }
+});
