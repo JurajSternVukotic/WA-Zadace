efb3e2377c9e1046d3fc14a88ff328e1d6303983
Juraj Štern-Vukotić <jsternvuk@unipu.hr>
2025-01-13 21:27:35 +0100

    add movies get

--- vjezbe_6_samostalni/routes/movies.js
+++ vjezbe_6_samostalni/routes/movies.js
@@ -1,7 +1,23 @@
 import express from "express";
 const router = express.Router();
+import fs from "fs/promises";
+import path from "path";
 
-router.get("/", async (req, res) => {});
+const dataPath = path.join(process.cwd(), "data", "movies.json");
+
+export async function readMoviesFile() {
+  const data = await fs.readFile(dataPath, "utf8");
+  return JSON.parse(data);
+}
+
+router.get("/", async (req, res) => {
+  try {
+    const movies = await readMoviesFile();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ error: "Error reading movies." });
+  }
+});
 router.get("/:id", async (req, res) => {});
 router.post("/", async (req, res) => {});
 router.patch("/", async (req, res) => {});
