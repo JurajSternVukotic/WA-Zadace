8ac59a3cd4348002c932e327400fe2540fe7f276
Juraj Štern-Vukotić <jsternvuk@unipu.hr>
2025-01-13 18:16:18 +0100

    primjer 1.3

--- vjezbe_6_samostalni/index.js
+++ vjezbe_6_samostalni/index.js
@@ -4,9 +4,40 @@ import korisniciRouter from "./routes/korisnici.js";
 const app = express();
 
 app.use(express.json());
+const timer = (req, res, next) => {
+  console.log(`Trenutno vrijeme: ${new Date().toLocaleString()}`);
+  next();
+};
+const requestLogger = (req, res, next) => {
+  const date = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${date}] : ${method} ${url}`);
+  next();
+};
+
+const adminLogger = (req, res, next) => {
+  console.log("Oprez! Pristigao zahtjev na /admin rutu");
+  next();
+};
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Greška na poslužitelju" });
+};
+
+app.use(errorHandler);
+app.use(requestLogger);
+app.use(timer);
 app.use("/korisnici", korisniciRouter);
+app.all("/admin", adminLogger);
+korisniciRouter.all("/admin", adminLogger);
 
 let PORT = 3000;
+
+app.get("/error", (req, res) => {
+  throw new Error("Simulirana greška na poslužitelju");
+});
 
 app.listen(PORT, (error) => {
   if (error) {
